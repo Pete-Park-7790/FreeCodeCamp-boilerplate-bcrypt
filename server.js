065bf8e603817e238f6fcba08cd4113ab2463197
Harshit Singhal <harshitsinghal7790@gmail.com>
2025-05-14 19:16:37 +0530

    Task-13

--- server.js
+++ server.js
@@ -12,9 +12,35 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  if (err) {
+    console.error(err); // log any errors
+    return;
+  }
 
+  console.log('Hashed password:', hash); // shows the bcrypt hash
+
+  // Compare correct password
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Correct password matches hash?', res); // should log: true
+  });
+
+  // Compare incorrect password
+  bcrypt.compare(someOtherPlaintextPassword, hash, (err, res) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Wrong password matches hash?', res); // should log: false
+  });
+});
 
 //END_ASYNC
+
 
 //START_SYNC
 
